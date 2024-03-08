c6b56a024fb2772c0f5434041692069e0333e10e
ondrejkoziak <ondrej.koziak@systemhousesolutions.com>
2024-03-08 22:46:42 +0100

    Add project files.

---
+++ SHS_CNC_APP/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SHS_CNC_APP.Utils;
+
+Console.WriteLine("SHS CNC Tester App");
+SerialPorts.PrintPortNames();
+SerialPorts.Equals("COM5", 11500);
---
+++ SHS_CNC_APP/Utils/SerialPorts.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SHS_CNC_APP.Utils
+{
+    public class SerialPorts
+    {
+        public static string[] GetPortNames()
+        {
+            return System.IO.Ports.SerialPort.GetPortNames();
+        }  
+        
+        public static void PrintPortNames()
+        {
+            string[] ports = GetPortNames();
+            Console.WriteLine("Available Ports:");
+            foreach (string port in ports)
+            {
+                Console.WriteLine(port);
+            }
+        }
+
+        public bool InitPort(string portName, int baudRate)
+        {
+            return true;
+        }   
+    }
+}
